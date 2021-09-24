4956d63116aa29e2120c84babd0c6c3a25ce2a12
airdipu <airdipu@gmail.com>
2021-09-24 10:26:35 +0300

    PCR manual selection of components

--- PCR_manual.m
+++ PCR_manual.m
@@ -1,31 +1,44 @@
 dataset = readtable('compactiv.dat');     % Read the .dat formate as a table
-data = zscore(table2array(dataset));      % Changing data table to array & standarization
-plot(data);                               % Checking data center
+data = table2array(dataset);              % Changing data table to array
+
+X = data(:, 1:21);                        % Computer systems activity
+Y = data(:, 22);                          % Usr data
 TF = ismissing(data);                     % checking the missing data
 tot = sum(TF);
 tot;                                      % If missing the sum >0
-x = data(:, 1:21);                        % Computer systems activity
-y = data(:, 22);                          % Usr data
-[n, p] = size(x);
-yc = y - mean(y); 
+x = zscore(X);                            % Standarised system activity
+y = zscore(Y);                            % Standarised Usr data
+plot(X);                                  % Checking data center
+[n, p] = size(x); 
 
 % apply PCA
 [PCALoadings, PCAScores, EigenVals, PCAVar] = pca(x, 'Economy', false);
 
 figure;
-plot(1:10, 100*cumsum(PCAVar(1:10))/sum(PCAVar(1:10)));
+plot(1:20, 100*cumsum(PCAVar(1:20))/sum(PCAVar(1:20)));
 xlabel('Number of Principal Component')
 ylabel('Explained Variance in x')
 
-% Fitting regresson
-betaPCR = regress(yc, PCAScores(:,1:9));
+% Get regression factors for each Principal Component
+betaPCR = regress(y, PCAScores(:,1:9));
+
+% Transform B's from PCs to Beta coefficient for actual variable
 betaPCR = PCALoadings(:,1:9)*betaPCR;
 betaPCR = [mean(y) - mean(x)*betaPCR; betaPCR];
 
 yfitPCR = [ones(n, 1) x]*betaPCR;
 
 figure;
-plot(yc, yfitPCR, 'bo');
+plot(y, yfitPCR, 'bo');
 xlabel('Observed Response');
 ylabel('Fitted Response');
+
+
+
+
+% Calculation
+TSS = sum(y.^2);
+RSS = sum((y-yfitPCR).^2);
+rsquaredPCR = 1- (RSS/TSS);
+
 
