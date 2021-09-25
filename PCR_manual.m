242698c5d04928a4b2b27bbcaa4abb457303c0fe
airdipu <airdipu@gmail.com>
2021-09-25 21:46:53 +0300

    PCR manual selection of components

--- PCR_manual.m
+++ PCR_manual.m
@@ -16,10 +16,10 @@ x = data(:, 1:21);                        % Computer systems activity
 y = data(:, 22);                          % Usr data
 
 plot(x);                                  % Checking data center
-[n1, p1] = size(x); 
+[n, p] = size(x); 
 
 % apply PCA
-[PCALoadings, PCAScores, EigenVals, PCAVar, Explained] = pca(xTrain, 'Economy', false);
+[PCALoadings, PCAScores, EigenVals, PCAVar, Explained] = pca(x, 'Economy', false);
 
 figure;
 plot(1:10, 100*cumsum(PCAVar(1:10))/sum(PCAVar(1:10)));
@@ -27,13 +27,13 @@ xlabel('Number of Principal Component')
 ylabel('Explained Variance in x')
 
 % Get regression factors for each Principal Component
-betaPCR = regress(yTrain, PCAScores(:,1:9));
+betaPCR = regress(y, PCAScores(:,1:9));
 
 % Transform B's from PCs to Beta coefficient for actual variable
 betaPCR = PCALoadings(:,1:9)*betaPCR;
-betaPCR = [mean(yTest) - mean(xTest)*betaPCR; betaPCR];
+betaPCR = [mean(y) - mean(x)*betaPCR; betaPCR];
 
-yfitPCR = [ones(n, 1) xTest]*betaPCR;
+yfitPCR = [ones(n, 1) x]*betaPCR;
 
 figure;
 plot(yTest, yfitPCR, 'bo');
@@ -41,8 +41,6 @@ xlabel('Observed Response');
 ylabel('Fitted Response');
 
 % Calculation
-TSS = sum(yTest.^2);
-RSS = sum((yTest - yfitPCR).^2);
+TSS = sum(y.^2);
+RSS = sum((y - yfitPCR).^2);
 rsquaredPCR = 1- (RSS/TSS);
-
-
