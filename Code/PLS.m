4e486e07e4656955df1ab211d4f7b4f8c7f059c9
airdipu <airdipu@gmail.com>
2021-10-02 22:17:41 +0300

    added PLSR code

--- Code/PLS.m
+++ Code/PLS.m
@@ -62,14 +62,14 @@ legend([a, b, c], {'PCR: Explained Variance in X', 'PLS: Explained Variance in X
 
 % Calculation of MSE for PCR
 [n p] = size(X);
-PCRmsep = sum(crossval(@pcrsse, X, y, 'KFold', 10), 1)/n;
+PCRmsep = sum(crossval(@pcrsse, X, y, 'KFold', 9), 1)/n;
 % Plotting MSE
 figure;
 title("Mean Squared Error");
 hold on
-a = plot(0:10, PCRmsep, 'b-o');
-b = plot(0:10, MSE1(1,:), 'r-o');
-c = plot(0:10, MSE1(2,:), 'k-o');
+a = plot(0:9, PCRmsep, 'b-o');
+b = plot(0:9, MSE1(1,:), 'r-o');
+c = plot(0:9, MSE1(2,:), 'k-o');
 xlabel("Number of Components");
 ylabel("Estimated Mean Squared Prediction Error");
 legend([a, b, c], {'PCR: MSE in X', 'PLS: MSE in X', 'PLS: MSE in y'})
